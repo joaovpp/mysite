d61369e1fbca31744b8aaa315e71784b23437fa0
joaovpp <joaovpp@hotmail.com>
2022-12-19 15:52:46 -0300

    feat: django post templates

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,8 @@ from blog import views
 
 
 urlpatterns = [
-    path('', views.PostView.as_view(), name='home')
+    path('', views.PostView.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
 
 
